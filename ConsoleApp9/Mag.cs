6e4e28eca668097e580a6c16e56ef92a8b583eb5
IlonaBekoeva <beklona30@gmail.com>
2023-10-24 12:06:37 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp9/Mag.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Magazin
+    {
+        private string namemag;
+        private string vladelec;
+        public string nametovar;
+        static protected int colvovs = 10;
+        public string Articul;
+        public Magazin(string articul)
+        {
+            Articul = articul;
+        }
+    }
+    class Ton:Magazin
+    {
+        List<string> firms = new List<string> { "vs", "lv", "catris" };
+        public string Firma { get; set; }
+        public Ton():base("Ton")
+        {
+
+        }
+        public Ton( string firma):base("Ton")
+        {
+            Firma = firma;
+        }
+        public void Nalichie()
+        {
+            foreach(string s in firms)
+            {
+                if(s==Firma)
+                {
+                    Console.WriteLine("есть в асортименте");
+                }
+                else
+                {
+                    Console.WriteLine("нет товара этой фирмы");
+                }
+            }
+        }
+        public void Pocupka(string what , int count)
+        {
+
+        }
+    }
+}
---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] at = new string[] { "Ton",""," "};
+            Magazin a = new Magazin("Ton");
+            if (a.Articul==at[0])
+            {
+                Ton ton = new Ton();
+                ton.Firma = Console.ReadLine();
+                ton.Nalichie();
+            }
+        }
+    }
+}
